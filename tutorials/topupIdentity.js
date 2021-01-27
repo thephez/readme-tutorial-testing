7fa1621ce7633a9fd64436fa86c7f0cf930f1d63
thephez <thephez@users.noreply.github.com>
2021-01-27 12:36:23 -0500

    feat: add topup

---
+++ tutorials/topupIdentity.js
@@ -0,0 +1,24 @@
+/* const Dash = require('dash');
+
+const clientOpts = {
+  network: 'testnet',
+  wallet: {
+    mnemonic: 'a Dash wallet mnemonic with testnet funds goes here',
+  },
+};
+const client = new Dash.Client(clientOpts); */
+
+async function topupIdentity(sdkClient, identityId) {
+  // const identityId = 'an identity ID goes here';
+  const topUpAmount = 1000; // Number of duffs
+
+  await sdkClient.platform.identities.topUp(identityId, topUpAmount);
+  return sdkClient.platform.identities.get(identityId);
+}
+
+/* topupIdentity()
+  .then((d) => console.log('Identity credit balance: ', d.balance))
+  .catch((e) => console.error('Something went wrong:\n', e))
+  .finally(() => client.disconnect()); */
+
+module.exports.topupIdentity = topupIdentity;
