bcb062c02dd3d9cbf21d1bdb5eb5318b463cb98c
thephez <thephez@users.noreply.github.com>
2021-03-08 09:53:05 -0500

    0.18 updates (#1)
    * chore: update dependencies to 0.18-dev
    
    * fix: update getIdentityIds to work with 0.18
    
    * chore: dependency update
    
    * ci: enable wallet test to run quickly by using offline mode
    
    * ci: use syncstartheight to speed up wallet sync
    
    * chore: update good IPs
    
    * chore: update dev dependencies
    
    * chore: update dpp to 0.18.0
    
    * chore: update dash to 0.18.0
    
    * ci: palinka test params
    
    * chore: update dash to 0.18.1
    
    * ci: update to ensure that name search is now case insensitive in 0.18
    
    * feat: add misc tests to complete tutorial coverage
    
    * feat: add second document to be retained
    
    * chore: switch back to testnet

---
+++ tutorials/sendFunds.js
@@ -0,0 +1,29 @@
+// const Dash = require('dash');
+
+// const clientOpts = {
+//   network: 'testnet',
+//   wallet: {
+//     mnemonic: 'your wallet mnemonic goes here',
+//     unsafeOptions: {
+//       skipSynchronizationBeforeHeight: 415000, // only sync from start of 2021
+//     },
+//   },
+// };
+// const client = new Dash.Client(clientOpts);
+
+async function sendFunds(client) {
+  const account = await client.getWalletAccount();
+
+  const transaction = account.createTransaction({
+    recipient: 'yP8A3cbdxRtLRduy5mXDsBnJtMzHWs6ZXr', // Testnet2 faucet
+    satoshis: 100, // 0.000001 Dash
+  });
+  return account.broadcastTransaction(transaction);
+}
+
+// sendFunds()
+//   .then((d) => console.log('Transaction broadcast!\nTransaction ID:', d))
+//   .catch((e) => console.error('Something went wrong:\n', e))
+//   .finally(() => client.disconnect());
+
+module.exports.sendFunds = sendFunds;
