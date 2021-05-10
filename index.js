f3300fc8bcd747646142552ed4e3ebfc5d3f26ce
thephez <thephez@users.noreply.github.com>
2021-05-10 10:15:00 -0400

    feat: get first wallet address

--- index.js
+++ index.js
@@ -1,5 +1,26 @@
 const Dash = require('dash');
+const dotenv = require('dotenv');
 const tutorials = require('./tutorials');
 
-const client = new Dash.Client();
+dotenv.config();
+const mnemonic = process.env.WALLET_MNEMONIC;
+
+async function getNewWalletInfo(sdkClient) {
+  const account = await sdkClient.getWalletAccount();
+  const address = account.getUnusedAddress();
+  console.log('Mnemonic:', mnemonic);
+  console.log('First account address:', address.address);
+  sdkClient.disconnect();
+}
+
+const client = new Dash.Client({
+  wallet: {
+    mnemonic,
+    offlineMode: true,
+  },
+});
+
 tutorials.checkNetworkConnection(client).then((x) => (console.log(x)));
+
+// Get first address of wallet
+getNewWalletInfo(client);
