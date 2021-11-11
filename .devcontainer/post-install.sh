8a30562c4fe9b069edb03af8de5fe4a65d88378c
thephez <thephez@users.noreply.github.com>
2021-11-11 15:53:13 +0000

    feat: automatically install dependencies when starting a new codespace

--- .devcontainer/post-install.sh
+++ .devcontainer/post-install.sh
@@ -1,2 +1,5 @@
 #  Use https instead of ssh for git (currently required for jawid-h/protobuf.js.git)
 git config --global url."https://github.com/".insteadOf ssh://git@github.com/
+
+# Install dependencies
+npm ci
